27eb1a6a7d852d203729c002e3194fe8e506c5c7
ej3131 <eunnnn31@gmail.com>
2025-06-21 23:29:49 +0900

    Update detect.py
    추가한 내용 주석 추가

--- detect.py
+++ detect.py
@@ -27,9 +27,11 @@ Usage - formats:
                                  yolov5s_edgetpu.tflite     # TensorFlow Edge TPU
                                  yolov5s_paddle_model       # PaddlePaddle
 """
+## 추가한 내용
 import pathlib
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
+###
 
 
 import argparse
